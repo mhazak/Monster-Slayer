8a1df72f96243f47a4d23f9d420e8451dd95e35e
Marek Hazák <marek.hazak@softigo.sk>
2022-07-31 16:12:53 +0200

    [dev] special attack

--- app.js
+++ app.js
@@ -7,26 +7,41 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
+            currentRound: 0
         }
     },
     computed: {
         playerBarStyles() {
             return { width: this.playerHealth + '%' };
         },
+
         monsterBarStyles() {
             return { width: this.monsterHealth + '%' };
+        },
+        
+        specialAttackAvailable() {
+            return this.currentRound % 3 !== 0;
         }
     },
     methods: {
         attackMonster() {
+            this.currentRound++;
             const damage = randomVal(5, 12);
             this.monsterHealth -= damage;
             this.attackPlayer();
         },
+        
         attackPlayer() {
             const damage = randomVal(8, 15);
             this.playerHealth -= damage;
             console.log({player: this.playerHealth, monster: this.monsterHealth});
+        },
+        
+        specialAttackMonster() {
+            this.currentRound++;
+            const damage = randomVal(10, 25);
+            this.monsterHealth -= damage;
+            this.attackPlayer();
         }
     }
 });
